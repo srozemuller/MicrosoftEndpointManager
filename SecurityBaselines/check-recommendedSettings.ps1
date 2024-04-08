19f461b157dba1f2bb8c694444b69863a0bf21fc
srozemuller <s.rozemuller@exite.com>
2024-04-08 15:01:46 +0200

    Create check-recommendedSettings.ps1

---
+++ SecurityBaselines/check-recommendedSettings.ps1
@@ -0,0 +1,50 @@
+$clientId = "14d82eec-204b-4c2f-b7e8-296a70dab67e" # Microsoft Graph Powershell enterprise application
+$token = Get-MsalToken -ClientId $clientId -Scopes "https://graph.microsoft.com/DeviceManagementConfiguration.Read.All"
+$header = @{
+    Authorization = "Bearer {0}" -f $token.AccessToken
+}
+
+# Search for all Edge security baselines
+$currentConfiguredBaselinesUrl = "https://graph.microsoft.com/beta//deviceManagement/configurationPolicies?`$filter=(templateReference/TemplateId%20eq%20%27c66347b7-8325-4954-a235-3bf2233dfbfd_1%27%20or%20templateReference/TemplateId%20eq%20%27c66347b7-8325-4954-a235-3bf2233dfbfd_2%27)%20and%20(templateReference/TemplateFamily%20eq%20%27Baseline%27)"
+$getCurrentBaselines = Invoke-RestMethod -Uri $currentConfiguredBaselinesUrl -Headers $header -Method Get
+
+# Then for each baseline, get the settings and check the insights against the insights API.
+# Settings are needed to get nice names for the settings.
+$getCurrentBaselines.value.ForEach
+({
+    # Code block here
+    $baseline = $_
+
+    # Get current baseline information
+    $baselineSettingsUrl = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies('{0}')/settings?`$expand=settingDefinitions&top=1000" -f $baseline.Id
+    $getBaselineSettings = Invoke-RestMethod -Uri $baselineSettingsUrl -Headers $header -Method Get
+
+    # Get setting insights for the baseline
+    $getSettingInsightsUrl = "https://graph.microsoft.com/beta/deviceManagement/templateInsights('{0}')/settingInsights" -f $baseline.templateReference.templateid
+    $getSettingInsights = Invoke-RestMethod -Uri $getSettingInsightsUrl -Headers $header -Method Get
+
+    # When there are insights available, loop through them and compare the current setting with the recommended setting.
+    if ($getSettingInsights.count -gt 0) {
+        $getSettingInsights.value.ForEach({
+                $getSettingInsightsId = $_.settingDefinitionId
+                $recommendedSettingId = $_.settingInsight.value
+
+                $currentBaselineDefinition = $getBaselineSettings.value.settingDefinitions.Where({ $_.id -eq $getSettingInsightsId })
+                $currentBaselineSetting = ($getBaselineSettings.value.settingInstance.Where({ $_.settingDefinitionId -eq $getSettingInsightsId })).choiceSettingValue.value
+
+                $currentBaselineSettingReadable = $currentBaselineDefinition.options.Where({ $_.itemId -eq $currentBaselineSetting }).displayName
+                $shouldBeValue = $currentBaselineDefinition.options.Where({ $_.itemId -eq $recommendedSettingId }).displayName
+
+                if ($currentBaselineSettingReadable -ne $shouldBeValue) {
+                    Write-Warning "Baseline: $($baseline.Name) has setting: $($currentBaselineDefinition.displayName) with value: $($currentBaselineSettingReadable) but should be: $shouldBeValue"
+                }
+                else {
+                    Write-Host "Baseline: $($baseline.Name) has setting: $($currentBaselineDefinition.displayName) with value: $($currentBaselineSettingReadable) and is correct."
+                }
+            })
+    }
+    else {
+        Write-Host "Baseline: $($baseline.Name) has no insights."
+    }
+})
+
